247d550eaeee4617030823f8d492795441e64914
AdvocateSIYA <siyabongagiftmbele99@gmail.com>
2023-09-08 11:09:18 -0700

    Third commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - Determines if a nimber is positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n - rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		prinf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		prinf("%d is zero\n", n);
+	}
+	else
+	{
+		prinf("%d is negative\n", n);
+	}
+	return (0);
