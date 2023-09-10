ee55b702f693255a66b04a636125cd74e9904dcd
AdvocateSIYA <siyabongagiftmbele99@gmail.com>
2023-09-10 15:09:16 -0700

    Hello World!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
 /**
  * main - Entry point
  *
@@ -9,5 +9,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
-	return 1;
+	return (1);
 }
