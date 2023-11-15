a5422bf8511d3a26fef62513ab5486c87edd2a07
AdvocateSIYA <siyabongagiftmbele99@gmail.com>
2023-11-15 13:11:16 -0800

    last one for the night

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		longint++;
-		longint += _strlen_recursion(s + 1);
+		count++;
+		count += _strlen_recursion(s + 1);
 	}
-	return (longint);
+	return (count);
 }
